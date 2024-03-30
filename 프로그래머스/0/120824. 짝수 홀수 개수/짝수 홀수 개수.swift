07c9ed94e161fd12bdc7048ec56fdc5e7b52a11b
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-03-30 11:01:12 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -1,21 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    var result: [Int] = []
-    var double = 0
-    var third = 0
-    
-    
-    for i in num_list {
-        if i % 2 == 0 {
-            double += 1
-        } else {
-            third += 1
-        }
-    }
-    
-    result.append(double)
-    result.append(third)
-    
-    return result
+    [num_list.filter { $0 % 2 == 0}.count, num_list.filter { $0 % 2 == 1 }.count]
 }

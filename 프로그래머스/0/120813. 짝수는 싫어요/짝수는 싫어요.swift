8f0ee9b0e5a14ccd6d26ceaa546731605be11127
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-04-05 09:41:22 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.swift
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    var result: [Int] = []
-    
-    for i in 1...n {
-        if i % 2 != 0 {
-            result.append(i)
-        }
-    }
-    return result
+    (1...n).filter { $0 % 2 == 1 }
 }

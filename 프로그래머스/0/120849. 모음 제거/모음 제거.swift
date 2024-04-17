0d8b4e0c7e47847728a3668f68b004a3848f9622
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-04-17 11:26:25 +0900

    [level 0] Title: 모음 제거, Time: 0.14 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.swift
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.swift
@@ -1,13 +1,7 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
-    let str: Set<Character> = ["a", "e", "i", "o", "u"]
-    var result = ""
     
-    for i in my_string {
-        if !str.contains(i) {
-            result.append(i)
-        }
-    }
-    return result
+    return my_string.filter { !["a", "e", "i", "o", "u"].contains($0) }
+    
 }

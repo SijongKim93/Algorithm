b9d007956e9af6325113b37600f37a809decb6a4
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-04-19 10:49:06 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ my_string:String, _ num1:Int, _ num2:Int) -> String {
+    var array = Array(my_string)
+    
+    array.swapAt(num1, num2)
+    
+    var result = String(array)
+    
+    return result
+}

9b692a0c9d8a13bb88e39d0a00ed557e32b64d4c
Si Jong <158182449+SijongKim93@users.noreply.github.com>
2024-05-06 11:47:55 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.06 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ strArr:[String]) -> [String] {
+    var result:[String] = []
+    
+    for (index, str) in strArr.enumerated() {
+        if index % 2 == 0 {
+            result.append(str.lowercased())
+        } else {
+            result.append(str.uppercased())
+        }
+    }
+    
+    return result
+}

b01ea70c0ccf567123ab3c48259bbe704f72adca
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-02-04 23:57:58 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.06 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.swift
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ a: Int, _ b: Int) -> Int {
-    let aDigits = String(a).count
-    let bDigits = String(b).count
-    
-    let ab = a * Int(pow(10.0, Double(bDigits))) + b
-    let ba = b * Int(pow(10.0, Double(aDigits))) + a
-    
-    return max(ab, ba)
+    return max(Int(String(a) + String(b))!, Int(String(b) + String(a))!)
 }

f4753ab5e2ad20f77adaa2ce7d18180e82d42022
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-18 23:03:16 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.swift
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.swift
@@ -1,15 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> Int {
-    var sumlist: Int = 0
-    var xlist: Int = 1
-    
-    for i in num_list {
-        if num_list.count >= 11 {
-            sumlist += i
-        } else if num_list.count <= 10 {
-            xlist *= i
-        }
-    }
-    return num_list.count >= 11 ? sumlist : xlist
+    return num_list.count >= 11 ? num_list.reduce(0, +) : num_list.reduce(1, *)
 }

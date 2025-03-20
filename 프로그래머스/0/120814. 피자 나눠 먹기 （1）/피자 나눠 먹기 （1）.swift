cc1cf3fcb94afd70492fad713df1d0a099fdd327
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-20 23:40:44 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.swift
@@ -1,5 +1,15 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    n % 7 == 0 ? n / 7 : n / 7 + 1
+    var result = 0
+    
+    if n <= 7 {
+        result = 1
+    } else if n % 7 == 0 {
+        result = n / 7
+    } else {
+        result = n / 7 + 1
+    }
+    
+    return result
 }

0f446aff2d15d7bd10ba887517d7978de927ce7e
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-22 00:12:19 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.swift
@@ -2,8 +2,12 @@ import Foundation
 
 func solution(_ my_string:String, _ n:Int) -> String {
     var result = ""
-    for char in my_string {
-        result += String(repeating: char, count: n)
+    
+    for i in my_string {
+        for _ in 1...n {
+            result += String(i)
+        }
     }
+    
     return result
 }

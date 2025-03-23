b34dbeefdd1983b4bfa2ac972704e72be2d64500
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-23 22:44:43 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.swift
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int], _ num1:Int, _ num2:Int) -> [Int] {
-    let numberStart: [Int] = Array(numbers[num1...num2])
-    return numberStart
+    return numbers[num1...num2].map { $0 }
 }

cad7dc923d27a7b5ef0fbeea29d83a56cb488d10
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-24 23:39:30 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.swift
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 func solution(_ my_string:String) -> String {
+    var strArray = ["a", "e", "i", "o", "u"]
     
-    return my_string.filter { !["a", "e", "i", "o", "u"].contains($0) }
-    
+    return my_string.filter { !strArray.contains(String($0)) }
 }

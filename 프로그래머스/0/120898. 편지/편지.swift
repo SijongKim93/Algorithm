9f8f0f9993ead3b2825d9b8e1081ae0dc44054d0
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-26 23:27:15 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.swift
+++ 프로그래머스/0/120898. 편지/편지.swift
@@ -2,8 +2,10 @@ import Foundation
 
 func solution(_ message:String) -> Int {
     var result = 0
-    for char in message {
-        result += 1
+    
+    for str in message {
+        result += 2
+        
     }
-    return result * 2
+    return result
 }

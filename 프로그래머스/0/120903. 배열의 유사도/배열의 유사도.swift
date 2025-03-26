302ec9a4ab1b44bd2d19c2c1a67996e6c4dce677
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-26 23:34:14 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.swift
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 func solution(_ s1:[String], _ s2:[String]) -> Int {
-    var count = 0
+    var result = 0
+    
     for i in s1 {
         if s2.contains(i) {
-            count += 1
+            result += 1
         }
     }
-    return count
+    
+    return result
 }

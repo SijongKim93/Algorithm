25331814d5f71db93a8b3b36cab2fc39ab5ee905
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-29 00:49:26 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.swift
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
-    var result:[Int] = []
-    
-    for i in numbers {
-        result.append(i * 2)
-    }
-            
-    return result
+    return numbers.map { $0 * 2 }
 }

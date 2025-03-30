dcb5bdb18274812941cadad6a8175ad1dc8136ff
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-30 19:29:34 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.swift
@@ -1,11 +1,11 @@
 import Foundation
 
-func solution(_ price:Int) -> Int {
-    if price >= 500000 {
+func solution(_ price: Int) -> Int {
+    if price >= 500_000 {
         return Int(Double(price) * 0.8)
-    } else if price >= 300000 {
+    } else if price >= 300_000 {
         return Int(Double(price) * 0.9)
-    } else if price >= 100000 {
+    } else if price >= 100_000 {
         return Int(Double(price) * 0.95)
     } else {
         return price

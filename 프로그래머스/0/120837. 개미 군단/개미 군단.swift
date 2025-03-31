26f9618064457d0d34221e62beb2805c41a544d8
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-03-31 22:01:09 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.swift
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.swift
@@ -1,18 +1,7 @@
 import Foundation
 
 func solution(_ hp:Int) -> Int {
-    let general = hp / 5
+    let result = hp % 5
     
-    if hp % 5 == 0 {
-        return general
-    }
-    
-    let remaining = hp % 5
-    let general2 = remaining / 3
-    
-    if remaining % 3 == 0 {
-        return general + general2
-    } else {
-        return general + general2 + (remaining % 3)
-    }
+    return hp / 5 + result / 3 + result % 3
 }

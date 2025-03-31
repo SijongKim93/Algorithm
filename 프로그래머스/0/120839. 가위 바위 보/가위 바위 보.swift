e2694274ef11b527ebae9232235ffafdc9d28ba7
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-04-01 00:35:39 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.swift
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.swift
@@ -1,21 +1,5 @@
 import Foundation
 
 func solution(_ rsp:String) -> String {
-    var r = "2"
-    var s = "0"
-    var p = "5"
-    
-    var result: String = ""
-    
-    for str in rsp {
-        if str == r.first! {
-            result.append(String(s.first!))
-        } else if str == s.first! {
-            result.append(String(p.first!))
-        } else {
-            result.append(String(r.first!))
-        }
-    }
-    
-    return result
+    return rsp.map { $0 == "0" ? "5" : $0 == "5" ? "2" : "0" }.joined()
 }

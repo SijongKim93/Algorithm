3cd233f55f218aba0dfe26c8c61745fd5706c548
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-04-05 22:35:03 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.09 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.swift
@@ -1,14 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String) -> Int {
-    var result = 0
-    
-    for i in 0...9 {
-        for str in my_string {
-            if String(str) == String(i) {
-                result += i
-            }
-        }
-    }
-    return result
+    return my_string.compactMap { Int(String($0)) }.reduce(0, +)
 }

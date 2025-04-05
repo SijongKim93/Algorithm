dd3a12e6589ab0a579a87afaf474216217bc0c36
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-04-05 22:20:33 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.swift
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.swift
@@ -1,9 +1,5 @@
 import Foundation
 
 func solution(_ box:[Int], _ n:Int) -> Int {
-    var result1 = box[0] / n
-    var result2 = box[1] / n
-    var result3 = box[2] / n
-    
-    return result1 * result2 * result3
+    return box.map { $0 / n}.reduce(1, *)
 }

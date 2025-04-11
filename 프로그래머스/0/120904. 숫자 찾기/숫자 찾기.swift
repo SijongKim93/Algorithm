aaafa978248226c0690f3b2f754bc0b5e7753fd1
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-04-11 23:31:41 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.swift
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.swift
@@ -1,11 +1,14 @@
 import Foundation
 
 func solution(_ num:Int, _ k:Int) -> Int {
-    let numString = String(num)
+    let strNum = String(num)
+    var arrayNum: [String] = []
     
-    if let index = numString.firstIndex(of: Character(String(k))) {
-        return numString.distance(from: numString.startIndex, to: index) + 1
-    } else {
-        return -1
+    for i in strNum {
+        arrayNum.append(String(i))
     }
+    
+    var count = Int(arrayNum.firstIndex(of: String(k)) ?? -2) + 1
+    
+    return count
 }

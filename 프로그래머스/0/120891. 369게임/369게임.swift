30e473c140c19a76a46e4826ee2f8d76939bd3f5
Si Jong Kim <158182449+SijongKim93@users.noreply.github.com>
2025-04-14 23:29:13 +0900

    [level 0] Title: 369게임, Time: 0.09 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.swift
+++ 프로그래머스/0/120891. 369게임/369게임.swift
@@ -1,18 +1,5 @@
 import Foundation
 
 func solution(_ order:Int) -> Int {
-    var strOrder = String(order)
-    var result = 0
-    
-    for char in strOrder {
-        if String(char) == "3" {
-            result += 1
-        } else if String(char) == "6" {
-            result += 1
-        } else if String(char) == "9" {
-            result += 1
-        }
-    }
-    
-    return result
+    return String(order).filter { $0 == "3" || $0 == "6" || $0 == "9" }.count
 }
